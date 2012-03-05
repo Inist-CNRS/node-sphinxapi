862cdf1d73893b79518d9767c7aaa6a3b976f7d5
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-03-05 23:21:43 +0100

    error handle ok

--- examples/test.js
+++ examples/test.js
@@ -4,9 +4,12 @@ var SphinxClient = require ("../lib/sphinxapi.js");
 
 var cl = new SphinxClient();
 //cl.SetServer('localhost', 51023);
-cl.SetServer('172.16.128.183',  51023);
-cl.Status(function(r) {
-		console.log(r)
+//cl.SetServer('172.16.128.183',  51023);
+cl.Status(function(err, data) {
+		if (err) {
+			throw err;
+		}
+		console.log(data);
 })
 
 //cl.AddQuery('test');
