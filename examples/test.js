2d6a182261809a63eb8960f5cc7c5d7f7d4b8343
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-03-06 22:56:53 +0100

    un peu plus

--- examples/test.js
+++ examples/test.js
@@ -1,17 +1,16 @@
 // #!/usr/bin/env node
 
-var SphinxClient = require ("../lib/sphinxapi.js");
+var SphinxClient = require ("../lib/sphinxapi.js"),
+	assert = require('assert');
 
 var cl = new SphinxClient();
 //cl.SetServer('localhost', 51023);
 //cl.SetServer('172.16.128.183',  51023);
-cl.Status(function(err, data) {
-		if (err) {
-			throw err;
-		}
-		console.log(data);
+cl.Status(function(err) {
+		assert.ifError(err);
+		console.log('Status is OK');
 })
 
-//cl.AddQuery('test');
+cl.AddQuery('test');
 //console.log(cl.RunQueries());
 
