bb9388672bc2238a6937ba5440a3c76c4ff0289e
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-04-25 22:48:33 +0200

    runqueries ported

--- examples/runqueries.js
+++ examples/runqueries.js
@@ -1,12 +1,13 @@
 // #!/usr/bin/env node
 
 var SphinxClient = require ("../lib/sphinxapi.js"),
+	util = require('util'),
 	assert = require('assert');
 
 var cl = new SphinxClient();
 cl.SetServer('localhost', 19312);
 var r = cl.AddQuery('test');
 cl.RunQueries(function(err, result) {
-		console.log(result);
+		console.log(console.log(util.inspect(result, false, null,true)));
 })
 
