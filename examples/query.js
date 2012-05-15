2c19128c7c9fe75797063b967a0ab00447a52bb9
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-05-15 10:33:34 +0200

    use util.inspect

--- examples/query.js
+++ examples/query.js
@@ -1,11 +1,13 @@
 // #!/usr/bin/env node
 
 var SphinxClient = require ("../lib/sphinxapi.js"),
+	util = require('util'),
 	assert = require('assert');
 
 var cl = new SphinxClient();
 cl.SetServer('localhost', 19312);
-cl.Query('test', function(err, res) { 
-		console.log(err, res);
+cl.Query('test', function(err, result) { 
+		assert.ifError(err);
+		console.log(util.inspect(result, false, null, true));
 });
 
--- examples/status.js
+++ examples/status.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 var SphinxClient = require ("../lib/sphinxapi.js"),
+	util = require('util'),
 	assert = require('assert');
 
 var cl = new SphinxClient();
 cl.SetServer('localhost', 19312);
 cl.Status(function(err, result) {
 		assert.ifError(err);
-		console.log(result);
+		console.log(util.inspect(result, false, null, true));
 })
